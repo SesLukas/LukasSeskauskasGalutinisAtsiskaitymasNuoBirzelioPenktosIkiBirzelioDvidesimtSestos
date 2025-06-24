565942d052f0a75a955a6dd5a7714b46dc5a466d
SesLukas <lukas.seskauskas@gmail.com>
2025-06-24 12:21:13 +0300

    pataisyti failai duomenų bazėje ir klausimų filtravimas

--- server/routes/questionsFilterRoutes.js
+++ server/routes/questionsFilterRoutes.js
@@ -1,11 +1,9 @@
 import express from "express";
-import {
-  getFilteredQuestions
-} from "../controllers/questionsFilterController.js";
+import { getAllQuestions } from "../controllers/questionsFilterController.js";
 
 const router = express.Router();
 
 
-router.get("/questions", getFilteredQuestions);
+router.get("/", getAllQuestions);
 
 export default router;
